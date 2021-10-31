081d6c03a95cf68b93c6e8bcefb6dad3a4464375
tvmichael <michael.tmv@outlook.com>
2021-10-31 19:17:26 +0200

    add html

--- js/function.js
+++ js/function.js
@@ -1,7 +1,5 @@
-
 // let func1 = new Function("return " + "function (a, b) { return a + b; }")();
 // console.log(func1(3,7));
-
 // let func2 = new Function("a", "b", "return a + b;");
 // console.log(func2(7,7));
 
