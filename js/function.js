c853bdf48e429f1c4d10f3ccb36b2879f83fbdac
tvmichael <michael.tmv@outlook.com>
2021-10-31 21:25:52 +0200

    add text

--- js/function.js
+++ js/function.js
@@ -1,13 +1,5 @@
-// let func1 = new Function("return " + "function (a, b) { return a + b; }")();
-// console.log(func1(3,7));
-// let func2 = new Function("a", "b", "return a + b;");
-// console.log(func2(7,7));
-
 let functionTextareaResult = document.getElementById('functionTextareaResult');
 let functionTextarea = document.getElementById('functionTextarea');
-
-console.log(functionTextarea);
-console.log(functionTextareaResult);
 
 function runTextFunction(tfun){
 	let n = 100;
